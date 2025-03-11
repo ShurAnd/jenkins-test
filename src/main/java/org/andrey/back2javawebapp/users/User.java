0868454c70889bb89e03493996d74a6254ed0c19
andrey shurmanov <shurmanovand@gmail.com>
2025-03-11 23:16:45 +0300

    add simple implementation for user and some fixes to jenkinsfile

---
+++ src/main/java/org/andrey/back2javawebapp/users/User.java
@@ -0,0 +1,26 @@
+package org.andrey.back2javawebapp.users;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Класс для описания пользователя приложения
+ */
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@JsonIgnoreProperties(ignoreUnknown = true)
+public class User {
+    private Long id = 0L;
+    private String username = "";
+    private String password = "";
+    private String firstName = "";
+    private String lastName = "";
+    private List<String> authorities = new ArrayList<>();
+    private int age = 0;
+}
